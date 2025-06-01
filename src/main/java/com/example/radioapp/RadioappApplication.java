3fe645b47df41269bce5d52d68a18ff3fcc9789d
RyuseiKojima <jiko.likegc@gmail.com>
2025-06-01 11:36:47 +0900

    開発スタート

---
+++ src/main/java/com/example/radioapp/RadioappApplication.java
@@ -0,0 +1,13 @@
+package com.example.radioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadioappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadioappApplication.class, args);
+	}
+
+}
