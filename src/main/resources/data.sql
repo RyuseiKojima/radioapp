e28040639b29528bbe3078a164f1fd1aea04d404
RyuseiKojima <jiko.likegc@gmail.com>
2025-06-04 22:56:03 +0900

    デフォルトデータの追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
+-- ラジオ局
 INSERT INTO radio_station (id, name) VALUES (1, 'ニッポン放送');
 INSERT INTO radio_station (id, name) VALUES (2, '文化放送');
 INSERT INTO radio_station (id, name) VALUES (3, 'TOKYO FM');
+
+-- ラジオ番組
+INSERT INTO radio_program (title, host, description, station_id) VALUES ('オードリーANN', 'オードリー', '', 1);
